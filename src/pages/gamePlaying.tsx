7ee8239bb3bfbf0a2f1f4c2d879bdf64f904356f
ray03j <eihouse.potato@outlook.jp>
2025-02-26 19:39:43 +0900

    feat: テーマ名表示追加

--- src/pages/gamePlaying.tsx
+++ src/pages/gamePlaying.tsx
@@ -5,9 +5,13 @@ import DefaultLayout from "@/layouts/default";
 
 export default function GamePlayingPage() {
   const {messages, sendMessage} = useWebSocket("ws://localhost:8080/ws");
-  
+  const theme = "エイくんが言いそうなこと"
   return (
     <DefaultLayout>
+      <h1 className="fixed top-14 left-1/2 -translate-x-1/2 text-xl font-bold bg-white/80 backdrop-blur p-2 rounded-lg shadow-md">
+        {theme}
+      </h1>
+
       <section className="flex flex-col items-start gap-4 py-8 md:py-10 max-w-xl mx-auto">
         <ChatList chatcards={messages} />
       </section>
