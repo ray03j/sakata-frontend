ee6b0be12dfc3d0aacb8e53005ddf796cf02e08f
ray03j <eihouse.potato@outlook.jp>
2025-02-27 01:09:31 +0900

    feat: overflow hidden

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -8,9 +8,9 @@ export default function DefaultLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="relative flex flex-col h-screen">
+    <div className="relative flex flex-col h-screen overflow-hidden">
       <Navbar />
-      <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
+      <main className="container mx-auto px-6 flex-grow pt-16">
         {children}
       </main>
       <footer className="w-full flex items-center justify-center py-3">
@@ -27,3 +27,5 @@ export default function DefaultLayout({
     </div>
   );
 }
+
+
