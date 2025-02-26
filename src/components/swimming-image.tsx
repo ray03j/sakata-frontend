b5ebadbbf6e13cf4462f0094c6bc7e112ac26611
ray03j <eihouse.potato@outlook.jp>
2025-02-27 01:25:22 +0900

    fix: swimming css

--- src/components/swimming-image.tsx
+++ src/components/swimming-image.tsx
@@ -9,9 +9,9 @@ const SwimmingImage = () => {
         alt="Swimming Ray"
         style={{position: 'absolute',width: '330px', height: 'auto'}}
         animate={{ 
-          x: [1000, 200, 100, -500], 
-          y: [1500, -100, 0, -1000], 
-          rotate: [0, 20, -40, 70] }}
+          x: [1500, 200, 100, -500], 
+          y: [1000, 300, 0, -1000], 
+          rotate: [0, -20, -40, 70] }}
         transition={{ duration: 4, ease: 'easeInOut', repeat: Infinity }}
       />
     </>
