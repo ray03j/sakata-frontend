1ce395236edff22db924670b58da4c43ced00800
ray03j <eihouse.potato@outlook.jp>
2025-02-26 23:18:55 +0900

    feat: ゲームタイトル決定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,11 +10,11 @@ export default function IndexPage() {
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
-          <span className={title()}>Make&nbsp;</span>
-          <span className={title({ color: "cyan" })}>Q</span>
+          <span className={title({ color: "cyan" })}>エイ</span>
+          <span className={title()}>くんのお気持ち</span>
           <br />
           <div className={subtitle({ class: "mt-4" })}>
-            わいわい&nbsp;どんどん出そう
+            
           </div>
         </div>
 
