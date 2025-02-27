7d93a415c13af85a3030dc05f31f3ac6fbbbf0d2
ray03j <eihouse.potato@outlook.jp>
2025-02-27 14:48:01 +0900

    fix: u userNameで判別して、チャットのUIを変更する

--- src/components/chat-card.tsx
+++ src/components/chat-card.tsx
@@ -3,17 +3,11 @@
 import { MessageType } from "@/types";
 import {Card, CardBody} from "@heroui/card";
 
-const user_id: string = import.meta.env.VITE_USER_ID!;
-
-export const ChatCard = ({userId, userName, message}: MessageType) => {
-
+export const ChatCard = ({ userName="AI", message}: MessageType) => {
   return (
     <div className="flex-col">
-      {userId===user_id ? 
-        <></> : <div>{userName}</div>
-      }
       <Card className="text-white m-1">
-        <CardBody className={userId===user_id ? "bg-green-500" : "bg-blue-500" }>
+        <CardBody className={userName==="AI" ? "bg-blue-500" : "bg-green-500" }>
           <p>{message}</p>
         </CardBody>
       </Card>
